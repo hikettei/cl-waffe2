9943101a83607298e358eb0af128d2873e5f3b14
hikettei <hikettei@gmail.com>
2023-05-25 13:06:35 +0900

    Added: CPU Backend

--- source/backends/cpu/tensor.lisp
+++ source/backends/cpu/tensor.lisp
@@ -1,5 +1,19 @@
 
 (in-package :cl-waffe2/backends.cpu)
 
-(defclass CPUTensor (AbstractTensor)
-  nil)
+(defclass CPUTensor (AbstractTensor) nil)
+
+(defmethod initialize-instance :before ((tensor CPUTensor)
+					&rest initargs
+					&key &allow-other-keys)
+  ;; if projected-p -> alloc new vec
+  (let ((shape (getf initargs :shape))
+	(dtype (dtype->lisp-type (getf initargs :dtype))))
+
+    ;; If tensor is created as View-Object, DONT alloc new tensor.
+
+    (setf (tensor-vec tensor)
+	  (make-array
+	   (apply #'* shape)
+	   :element-type dtype))))
+
