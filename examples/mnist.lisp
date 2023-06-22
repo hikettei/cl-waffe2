a6446d06dedb2ded7d166762788129deb5c5eb64
hikettei <hikettei@gmail.com>
2023-06-22 13:21:44 +0900

    [Add] Added Simple MLP as a example

---
+++ examples/mnist.lisp
@@ -0,0 +1,40 @@
+(in-package :cl-user)
+
+(defpackage :mnist-example
+  (:use
+   :cl
+   :cl-waffe2
+   :cl-waffe2/nn
+   :cl-waffe2/distributions
+   :cl-waffe2/base-impl
+   :cl-waffe2/vm.generic-tensor
+   :cl-waffe2/vm.nodes))
+
+(in-package :mnist-example)
+
+;; To ADD:
+;; call->
+;; sequence
+;; deftrainer
+
+;; TO ADD:
+;; (call-> x layer1 layer2 layer3)
+;; (x = (sequence (LinearLayer1 (LinearLayer2 ...
+
+;; ugly...
+(defmodel (MLP-Model (self)
+	   :slots ((layer1 :initarg :layer1 :accessor mlp-layer1)
+		   (layer2 :initarg :layer2 :accessor mlp-layer2)
+		   (layer3 :initarg :layer3 :accessor mlp-layer3))
+	   :initargs (:layer1 (LinearLayer 784 512)
+		      :layer2 (LinearLayer 512 256)
+		      :layer3 (LinearLayer 256 10))
+	   :on-call-> ((self x)
+		       (call (mlp-layer3 self)
+			     (call (mlp-layer2 self)
+				   (call (mlp-layer1 self) x))))))
+
+
+(let ((model (MLP-Model)))
+  (print (proceed-backward (!sum (call model (randn `(10 784)))))))
+
