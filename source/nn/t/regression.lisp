8a12b2b1f68e45c382b7669374029dfacc5ed50f
hikettei <hikettei@gmail.com>
2023-07-15 14:58:35 +0900

    defmodel produces NaN

--- source/nn/t/regression.lisp
+++ source/nn/t/regression.lisp
@@ -20,8 +20,8 @@
 	     (LinearLayer out-features hidden-size) ;; 2th
 	     (asnode #'!relu)
 	     (LinearLayer hidden-size out-features) ;; 3th
-	     (asnode #'!sigmoid)
-	     (asnode #'!sum))
+	     (asnode #'!relu)
+	     (asnode #'!mean))
 
 
 (defsequence LinearLayer-Sequence1 (in-features hidden-size out-features)
@@ -36,6 +36,90 @@
 (defun not-zero-p (tensor)
   (some #'(lambda (x) (not (= x 0))) (tensor-vec (grad tensor))))
 
+;; Chain Rule is Really Working?
+(defun matmul-chain-test ()
+  (let ((a (parameter (randn `(100 100))))
+	(b (parameter (randn `(100 100))))
+	(c (parameter (randn `(100 100)))))
+    (proceed-backward (!matmul a (!matmul b c)))
+    (and (not-zero-p a)
+	 (not-zero-p b)
+	 (not-zero-p c))))
+
+(defun matmul-chain-test1 ()
+  (let ((a (parameter (randn `(100 100))))
+	(b (parameter (randn `(100 100))))
+	(c (parameter (randn `(100 100)))))
+    (proceed-backward (!matmul a (!relu (!matmul b c))))
+    (and
+     (not-zero-p a)
+     (not-zero-p b)
+     (not-zero-p c))))
+
+(defun matmul-bias-test ()
+  (let ((a (parameter (randn `(100 100))))
+	(b (parameter (randn `(100 100))))
+	(c (parameter (randn `(100)))))
+    (proceed-backward (!sum (cl-waffe2/nn::step-linear a b c)))
+    (and
+     (not-zero-p a)
+     (not-zero-p b)
+     (every #'(lambda (x) (= x 1.0)) (tensor-vec (grad c))))))
+
+;; Linear(ReLU(Linear(x)))
+(defun linear-chain-test ()
+  (let ((a1 (parameter (randn `(100 100))))
+	(b1 (parameter (randn `(100 100))))
+	(c1 (parameter (randn `(100))))
+	(a2 (parameter (randn `(100 100))))
+	(c2 (parameter (randn `(100)))))
+    (proceed-backward (!mean
+		       (cl-waffe2/nn::step-linear
+			a2
+			(!relu (cl-waffe2/nn::step-linear a1 b1 c1))
+			c2)))
+    (and
+     (not-zero-p a1)
+     (not-zero-p b1)
+     (not-zero-p c1)
+     (not-zero-p a2)
+     (every #'(lambda (x) (= x 1e-4)) (tensor-vec (grad c2))))))
+
+;; Doing same things with build
+(defun linear-chain-test-build ()
+  (let ((a1 (parameter (randn `(100 100))))
+	(b1 (parameter (randn `(100 100))))
+	(c1 (parameter (randn `(100))))
+	(a2 (parameter (randn `(100 100))))
+	(c2 (parameter (randn `(100)))))
+    (let ((model (build (!mean
+			 (cl-waffe2/nn::step-linear
+			  a2
+			  (!relu (cl-waffe2/nn::step-linear a1 b1 c1))
+			  c2)))))
+      (forward model)
+      (backward model)
+      (and
+       (not-zero-p a1)
+       (not-zero-p b1)
+       (not-zero-p c1)
+       (not-zero-p a2)
+       (every #'(lambda (x) (= x 1e-4)) (tensor-vec (grad c2)))))))
+
+;; The bug occurs called with defmodel!.
+(defun linear-composite-test ()
+  (let ((model  (LinearLayer 100 10))
+	(model1 (LinearLayer 10 3)))
+    (let ((compiled-model (build (!mean (call model1 (call model (randn `(10 100))))))))
+      (forward compiled-model))))
+
+(test chain-rule-test-matmul
+  (is (matmul-chain-test))
+  (is (matmul-chain-test1))
+  (is (matmul-bias-test))
+  (is (linear-chain-test))
+  (is (linear-chain-test-build)))
+
 (defmacro with-model-parameters ((bind model) &body body)
   `(let ((,bind (nodevariables-parameters
 		 (compiled-variables ,model))))
@@ -47,14 +131,20 @@
 ;;
 ;; Only using pure features in cl-waffe2.
 (defun linearlayer-backward-test ()
-  (let* ((model (LinearLayer-Sequence 100 50 10))
-	 (model (build (call model (randn `(10 100))))))
-    (forward model)
-    (backward model)
-    (with-model-parameters (params model)
-      (every #'not-zero-p params))))
+  (with-memory-pool
+    (let* ((model (LinearLayer-Sequence 100 50 10))
+	   (model (build (call model (uniform-random `(10 100) -0.01 0.01))
+			 :compile-mode :default)))
+      (forward model)
+      (backward model)
+      (with-model-parameters (params model)
+	(loop for p in params
+	      do (print (grad p)))
+	(every #'not-zero-p params)))))
 
 (test linear-backward-test-only-with-principle-features
+  (is (linearlayer-backward-test))
+  (is (linearlayer-backward-test))
   (is (linearlayer-backward-test)))
 
 ;; Second Case:
@@ -75,9 +165,8 @@
     (print (forward model))
    ;; (backward model)
     (with-model-parameters (params model)
-      (print params)
       (every #'not-zero-p params)))))
 
-(test linearlayer-backward-with-criterion
+(test linearlayer-backward-with-criterlion
   (is (linearlayer-backward-test-with-criterion)))
 	     
