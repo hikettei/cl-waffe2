973ea4ba9eb74c71efcc19cbcfd78400d1bd8052
hikettei <hikettei@gmail.com>
2023-08-03 18:47:58 +0900

    Profiling JIT Compiler

--- benchmarks/profile.lisp
+++ benchmarks/profile.lisp
@@ -2,3 +2,20 @@
 (in-package :cl-waffe2/benchmark)
 
 
+
+(defsequence MLP-Sequence (in-features hidden-dim out-features
+			   &key (activation #'!relu))
+	     "Three Layers MLP Model"
+	     (LinearLayer in-features hidden-dim)
+	     (asnode activation)
+	     (LinearLayer hidden-dim hidden-dim)
+	     (asnode activation)
+	     (LinearLayer hidden-dim out-features))
+
+
+(defun build-mlp ()
+  (build
+   (!mean (softmax-cross-entropy
+	   (call (MLP-Sequence 100 10 5) (make-input `(batch-size 100) :X))
+	   (make-input `(batch-size ,5) :Y)))))
+
