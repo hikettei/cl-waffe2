d5db79726d154d01fd3d3a60135c68ca7272782f
hikettei <hikettei@gmail.com>
2023-08-07 20:43:40 +0900

    [Add] save MNIST training data as a npy format

---
+++ examples/mnist/train_data.py
@@ -0,0 +1,36 @@
+
+# This script downloads MNIST datum and saves as npy format
+# cl-waffe2 loads the file later.
+
+from torchvision.datasets import MNIST
+import numpy as np
+
+mnist_train = MNIST('~/tmp/mnist', train=True, download=True)
+mnist_test  = MNIST('~/tmp/mnist', train=False, download=True)
+
+train_set = mnist_train.data.numpy()
+test_set  = mnist_test.data.numpy()
+
+train_label = mnist_train.targets.numpy()
+test_label  = mnist_test.targets.numpy()
+
+print(train_set.shape)
+print(test_set.shape)
+
+def one_hot (array):
+    n_labels = len(np.unique(array))
+    return np.eye(n_labels)[array]
+
+print(one_hot(train_label).shape)
+print(one_hot(test_label).shape)
+
+import os
+
+os.mkdir("./data")
+
+np.save("./data/train_data.npy", train_set)
+np.save("./data/test_data.npy",  test_set)
+
+np.save("./data/train_label.npy", one_hot(train_label))
+np.save("./data/test_label.npy",  one_hot(test_label))
+
