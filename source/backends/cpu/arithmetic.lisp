7701270db482ea3dedeb467537f4c35bd83f752c
hikettei <hikettei@gmail.com>
2023-08-13 14:51:47 +0900

    [Fix] use gensym instead-of xxx-ptr names

--- source/backends/cpu/arithmetic.lisp
+++ source/backends/cpu/arithmetic.lisp
@@ -3,7 +3,7 @@
 
 ;; BLAS -> Lisp
 
-(defun expand-axpy-form (x y &key (alpha 1.0))
+(defun expand-axpy-form (x y x-ptr y-ptr &key (alpha 1.0))
   (let ((dtype (dtype x)))
     (call-with-view
      #'(lambda (x-view y-view)
@@ -13,24 +13,24 @@
 	    `(blas-saxpy
 	      ,(size-of y-view 0)
 	      ,alpha
-	      (incf-tensor-ptr ,y y-ptr :offset ,(offset-of y-view 0))
+	      (incf-tensor-ptr ,y ,y-ptr :offset ,(offset-of y-view 0))
 	      ,(stride-of y-view 0)
-	      (incf-tensor-ptr ,x x-ptr :offset ,(offset-of x-view 0))
+	      (incf-tensor-ptr ,x nx-ptr :offset ,(offset-of x-view 0))
 	      ,(stride-of x-view 0)))
 	   (:double
 	    `(blas-daxpy
 	      ,(size-of y-view 0)
 	      ,(coerce alpha 'double-float)
-	      (incf-tensor-ptr ,y y-ptr :offset ,(offset-of y-view 0))
+	      (incf-tensor-ptr ,y ,y-ptr :offset ,(offset-of y-view 0))
 	      ,(stride-of y-view 0)
-	      (incf-tensor-ptr ,x x-ptr :offset ,(offset-of x-view 0))
+	      (incf-tensor-ptr ,x ,x-ptr :offset ,(offset-of x-view 0))
 	      ,(stride-of x-view 0)))
 	   (T
 	    (error "the dtype ~a is not supported. (TODO)" dtype))))
      `(,x ,y)
      :fuse t)))
 
-(defun expand-move-form (x y)
+(defun expand-move-form (x y x-ptr y-ptr)
   (let ((dtype (dtype x)))
     (call-with-view
      #'(lambda (x-view y-view)
@@ -39,16 +39,16 @@
 	   (:float
 	    `(blas-scopy
 	      ,(size-of y-view 0)
-	      (incf-tensor-ptr ,y y-ptr :offset ,(offset-of y-view 0))
+	      (incf-tensor-ptr ,y ,y-ptr :offset ,(offset-of y-view 0))
 	      ,(stride-of y-view 0)
-	      (incf-tensor-ptr ,x x-ptr :offset ,(offset-of x-view 0))
+	      (incf-tensor-ptr ,x ,x-ptr :offset ,(offset-of x-view 0))
 	      ,(stride-of x-view 0)))
 	   (:double
 	    `(blas-dcopy
 	      ,(size-of y-view 0)
-	      (incf-tensor-ptr ,y y-ptr :offset ,(offset-of y-view 0))
+	      (incf-tensor-ptr ,y ,y-ptr :offset ,(offset-of y-view 0))
 	      ,(stride-of y-view 0)
-	      (incf-tensor-ptr ,x x-ptr :offset ,(offset-of x-view 0))
+	      (incf-tensor-ptr ,x ,x-ptr :offset ,(offset-of x-view 0))
 	      ,(stride-of x-view 0)))
 	   (T
 	    (error "the dtype ~a is not supported. (TODO)" dtype))))
@@ -58,32 +58,40 @@
 (define-impl (AddNode :device CPUTensor
 	      :reject-p (supported-dtypes-are 0 :float :double))
 	     :forward ((self x y)
-		       `(with-tensor-ptrs ((x-ptr ,x)
-					   (y-ptr ,y))
+		       (let ((x-ptr (gensym "PTR"))
+			     (y-ptr (gensym "PTR")))
+			 `(with-tensor-ptrs ((,x-ptr ,x)
+					     (,y-ptr ,y))
 			  (locally (declare (optimize (speed 1)))
-			    ,(expand-axpy-form x y)
-			    ,x))))
+			    ,(expand-axpy-form x y x-ptr y-ptr)
+			    ,x)))))
 
 (define-impl (SubNode :device CPUTensor
 	      :reject-p (supported-dtypes-are 0 :float :double))
 	     :forward ((self x y)
-		       `(with-tensor-ptrs ((x-ptr ,x)
-					   (y-ptr ,y))
-			  (locally (declare (optimize (speed 1)))
-			    ,(expand-axpy-form x y :alpha -1.0)
-			    ,x))))
+		       (let ((x-ptr (gensym "PTR"))
+			     (y-ptr (gensym "PTR")))
+			 `(with-tensor-ptrs ((,x-ptr ,x)
+					     (,y-ptr ,y))
+			    (locally (declare (optimize (speed 1)))
+			      ,(expand-axpy-form x y x-ptr y-ptr :alpha -1.0)
+			      ,x)))))
 
-;; MulNode/DivNode -> LispKernel
+;; TODO: Mul/Div, ScalarXX Ops with AVX2/AVX512
+;; Current State: MulNode/DivNode -> LispKernel
+
 (define-impl (MoveTensorNode :device CPUTensor
 			     :reject-p (supported-dtypes-are 0 :float :double))
 	     :forward ((self x y)
 		       ;; X <- Y
-		       `(with-tensor-ptrs ((x-ptr ,x)
-					   (y-ptr ,y))
-			  (locally (declare (optimize (speed 1)))
-			    (if (not (movetensor-ignore-me ,self))
-				(progn
-				  ,(expand-move-form x y)
-				  ,x)
-				,y)))))
+		       (let ((x-ptr (gensym "PTR"))
+			     (y-ptr (gensym "PTR")))
+			 `(with-tensor-ptrs ((,x-ptr ,x)
+					     (,y-ptr ,y))
+			    (locally (declare (optimize (speed 1)))
+			      (if (not (movetensor-ignore-me ,self))
+				  (progn
+				    ,(expand-move-form x y x-ptr y-ptr)
+				    ,x)
+				  ,y))))))
 
--- source/backends/cpu/matrix-ops.lisp
+++ source/backends/cpu/matrix-ops.lisp
@@ -20,6 +20,7 @@
 ;; FixME: support row-major with gemm
 
 (defun expand-gemm-form (a1 b1 c
+			 a-ptr b-ptr o-ptr
 			 &key
 			   trans-a?
 			   trans-b?)
@@ -80,14 +81,14 @@
 		;; variables that didn't appear in arguments
 		;; Is ignored, so (read-untransposed b) is needed to be lazily evaluated.
 		
-	        (incf-tensor-ptr ,b1 b-ptr :offset ,(offset-of b-view 0)) ;; no matter which dim=0, dim=1, offsets are common.
+	        (incf-tensor-ptr ,b1 ,b-ptr :offset ,(offset-of b-view 0)) ;; no matter which dim=0, dim=1, offsets are common.
 		,ldb
-		(incf-tensor-ptr ,a1 a-ptr :offset ,(offset-of a-view 0))
+		(incf-tensor-ptr ,a1 ,a-ptr :offset ,(offset-of a-view 0))
 		,lda
 		0.0
 		(incf-tensor-ptr
 		 ,c
-		 o-ptr
+		 ,o-ptr
 		 :offset ,(offset-of c-view 0))
 		,ldc))
 	     (:double
@@ -104,18 +105,18 @@
 		
 		(incf-tensor-ptr
 		 ,b1
-		 b-ptr
+		 ,b-ptr
 		 :offset ,(offset-of b-view 0)) ;; no matter which dim=0, dim=1, offsets are common.
 		,ldb
 		(incf-tensor-ptr
 		 ,a1
-		 a-ptr
+		 ,a-ptr
 		 :offset ,(offset-of a-view 0))
 		,lda
 		0.0d0
 		(incf-tensor-ptr
 		 ,c
-		 o-ptr
+		 ,o-ptr
 		 :offset ,(offset-of c-view 0))
 		,ldc))
 	     (T
@@ -131,17 +132,21 @@ Please consider using another backends." dtype)))))
 	     :forward
 	     ((self a b out)
 	      (let ((trans-a (trans-a? self))
-		    (trans-b (trans-b? self)))
+		    (trans-b (trans-b? self))
+		    (a-ptr   (gensym "PTR"))
+		    (b-ptr   (gensym "B"))
+		    (o-ptr   (gensym "OUT")))
 		`(locally (declare (optimize (speed 1)))
-		   (with-tensor-ptrs ((a-ptr ,a)
-				      (b-ptr ,b)
-				      (o-ptr ,out))
-		     ,(expand-gemm-form a b out :trans-a? trans-a :trans-b? trans-b)
+		   (with-tensor-ptrs ((,a-ptr ,a)
+				      (,b-ptr ,b)
+				      (,o-ptr ,out))
+		     ,(expand-gemm-form a b out a-ptr b-ptr o-ptr :trans-a? trans-a :trans-b? trans-b)
 		     ;; Sometime matmul fails due to wrong arguments
 		     ;; But proceeds with no errors...
 		     ,out)))))
 
-(defun expand-arg-maxmin-form (x out type &aux (index (gensym)))
+;; [TODO] Optimize to get more speed!
+(defun expand-arg-maxmin-form (x out type x-ptr &aux (index (gensym)))
   (declare (type (and keyword (member :max :min)) type))
   `(let (;;(x-vec (tensor-vec ,x))
 	 (o-vec (tensor-vec ,out))
@@ -169,7 +174,7 @@ Please consider using another backends." dtype)))))
 				 (:min 'blas-ismin)))
 			      (t (error "CPUTensor: argmax/argmin/max/min do not support ~a. only :float and :double" (dtype x))))
 			   ,(size-of x-view 0)
-			   (incf-tensor-ptr ,x x-ptr :offset ,(offset-of x-view 0))
+			   (incf-tensor-ptr ,x ,x-ptr :offset ,(offset-of x-view 0))
 			   ,(stride-of x-view 0)))))
 	      (incf ,index ,(stride-of o-view 0))))
        `(,x ,out)
@@ -178,20 +183,22 @@ Please consider using another backends." dtype)))))
 
 (define-impl (ArgMax-Node :device CPUTensor)
 	     :forward ((self x out)
-		       `(with-tensor-ptr (x-ptr ,x)
-			  (locally (declare (optimize (speed 1)))
-			    ,(expand-arg-maxmin-form x out :max)
-			    ,out))))
+		       (let ((x-ptr (gensym "PTR")))
+			 `(with-tensor-ptr (,x-ptr ,x)
+			    (locally (declare (optimize (speed 1)))
+			      ,(expand-arg-maxmin-form x out :max x-ptr)
+			      ,out)))))
 
 (define-impl (ArgMin-Node :device CPUTensor)
 	     :forward ((self x out)
-		       `(with-tensor-ptr (x-ptr ,x)
-			  (locally (declare (optimize (speed 1)))
-			  ,(expand-arg-maxmin-form x out :min)
-			  ,out))))
+		       (let ((x-ptr (gensym "PTR")))
+			 `(with-tensor-ptr (,x-ptr ,x)
+			    (locally (declare (optimize (speed 1)))
+			      ,(expand-arg-maxmin-form x out :min x-ptr)
+			      ,out)))))
 
 ;; [TODO] Force it using SIMD, by writing C kernel directly.
-(defun expand-maxmin-value-form (x out type &aux (index (gensym)))
+(defun expand-maxmin-value-form (x out type x-ptr &aux (index (gensym)))
   (declare (type (and keyword (member :max :min)) type))
   `(let ((x-vec (tensor-vec ,x))
 	 (o-vec (tensor-vec ,out))
@@ -223,7 +230,7 @@ Please consider using another backends." dtype)))))
 					 (:min 'blas-ismin)))
 				      (t (error "CPUTensor: argmax/argmin/max/min do not support ~a. only :float and :double" (dtype x))))
 				    ,(size-of x-view 0)
-				    (incf-tensor-ptr ,x x-ptr :offset ,(offset-of x-view 0))
+				    (incf-tensor-ptr ,x ,x-ptr :offset ,(offset-of x-view 0))
 				    ,(stride-of x-view 0))))))))
 		(incf ,index ,(stride-of o-view 0))))
 	 `(,x ,out)
@@ -232,15 +239,17 @@ Please consider using another backends." dtype)))))
 
 (define-impl (MaxValue-Node :device CPUTensor)
 	     :forward ((self x out)
-		       `(with-tensor-ptr (x-ptr ,x)
-			  (locally (declare (optimize (speed 1)))
-			    ,(expand-maxmin-value-form x out :max)
-			    ,out))))
+		       (let ((x-ptr (gensym "PTR")))
+			 `(with-tensor-ptr (,x-ptr ,x)
+			    (locally (declare (optimize (speed 1)))
+			      ,(expand-maxmin-value-form x out :max x-ptr)
+			      ,out)))))
 
 (define-impl (MinValue-Node :device CPUTensor)
 	     :forward ((self x out)
-		       `(with-tensor-ptr (x-ptr ,x)
-			  (locally (declare (optimize (speed 1)))
-			    ,(expand-maxmin-value-form x out :min)
-			    ,out))))
+		       (let ((x-ptr (gensym "PTR")))
+			 `(with-tensor-ptr (,x-ptr ,x)
+			    (locally (declare (optimize (speed 1)))
+			      ,(expand-maxmin-value-form x out :min x-ptr)
+			      ,out)))))
 
