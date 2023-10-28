16339bb110a1ab3726a67ee2c7537a19962919f3
hikettei <hikettei@gmail.com>
2023-10-28 21:56:25 +0900

    [Features] Commented out unused lines related to Block IR

--- source/vm/control-flow.lisp
+++ source/vm/control-flow.lisp
@@ -7,6 +7,9 @@
 ;;    MapNode(Block1, Block2)
 
 ;; [TODO] inside of *block* error messaging
+#|
+Not tested yet but this feature should be implemented in the future release
+so ive decide to leave this code as it is.
 (defparameter *block* nil)
 (defun make-block (iseq)
   (let ((block-name (gensym "BLOCK")))
@@ -23,4 +26,4 @@
 	       (format out "    ~a~%" i)))
 	   (format out "}")))
      nil)))
-
+|#
